ee8d20404f05eb7c9a194fbd6bae4d4030906925
monicakornis <2011mkornis@gmail.com>
2018-06-03 23:33:55 -0400

    style progress bar and modal

--- src/client/components/modal.jsx
+++ src/client/components/modal.jsx
@@ -16,15 +16,49 @@ const noop = () => {};
  };
 
 
-const Modal = ({ closeModal, todos }) => {
-  let numActive = todos.filter(obj => obj.status === 'active');
-  let total = todos.filter(obj => obj.status === 'active');
-  let percentCompleted;
+const Modal = ({ toggleModal, todos }) => {
+  let activeTodos = todos.filter((obj,i) => obj.status === 'active');
+  let completedTodos = todos.filter(obj => obj.status === 'complete' && obj.archive !== true);
+  let numCompleted = completedTodos.length;
+  completedTodos = completedTodos.map(obj =>
+                      <div className='completed-item'><h4>{obj.text}</h4></div>);
+  activeTodos = activeTodos.map((obj,i) =>
+                      <div key={i} className='completed-item'><h4>{obj.text}</h4></div>);
+  let total = todos.filter(obj => obj.archive !== true).length;
+  let percentCompleted = (numCompleted/total) * 100;
+
+  let styles = {
+    width: `${percentCompleted}%`
+  };
+
+  debugger
 
     return (
-      <div className='modalBackground' onClick={closeModal}>
+      <div className='modalBackground' onClick={toggleModal}>
         <div className='modal'>
-          MODAL
+
+        <div className="progressContainer">
+            <h1 className="text-center">Your Progress</h1>
+
+            <div className="completeProgressBar">
+              <div className="progressBars">
+                <div className="progress-bar" style={styles}></div>
+                <div className="progress-background"></div>
+              </div>
+              <div className="progress-shadow"></div>
+            </div>
+
+          </div>
+
+          <section className='textContainer'>
+            <div className='activeList'>
+              {activeTodos}
+            </div>
+            <div className='completedList'>
+              {completedTodos}
+            </div>
+          </section>
+
         </div>
       </div>
     );
